689cceeeb830b7c0411be8125bf47407a27fc1ea
strdoffin <undefu5624@gmail.com>
2025-12-09 16:07:40 +0700

    lab3 pdf

---
+++ lab2/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String args[]) {
+        char n1 = 'a';
+        if (Math.abs(3.2 - 3.2) < 0.001){
+            System.out.println("==");
+        }
+    }
+}
---
+++ lab3/src/test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author doffi
+ */
+public class test {
+    public static void main(String[] args) {
+        int cnt = 0;
+        for (int i = -10; i < 10 ; i-=10){
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
